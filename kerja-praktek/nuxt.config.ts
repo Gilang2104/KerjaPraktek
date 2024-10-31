d056f22d939039c94c8c464ede918792b9fa9304
Gilang2104 <gilangp2104@gmail.com>
2024-10-31 22:00:31 +0700

    nuxt.config.ts

--- kerja-praktek/nuxt.config.ts
+++ kerja-praktek/nuxt.config.ts
@@ -1,17 +1,17 @@
 import Aura from '@primevue/themes/aura';
 
 export default defineNuxtConfig({
-    components : true,
+  components: true,
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
-  modules:['@nuxtjs/tailwindcss', '@primevue/nuxt-module'],
-  plugins:['./plugins/index.js'],
+  modules: ['@nuxtjs/tailwindcss', '@primevue/nuxt-module'],
+  plugins: ['./plugins/index.js'],
   primevue: {
     options: {
-        theme: {
-            preset: Aura
-        }
-    }
-}
-
-})
+      theme: Aura,
+    },
+  },
+  nitro: {
+    preset: 'vercel', // Menyesuaikan Nuxt agar berjalan optimal di Vercel
+  },
+});
